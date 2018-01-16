84272348defd94398ec4bccc051b16bff341c21d
msa1111 <770127905@qq.com>
2018-01-16 18:22:13 +0800

    提交react-native的DEMO

---
+++ constant/UrlUtils.js
@@ -0,0 +1,19 @@
+/**
+ * 存储url的工具类
+ */
+
+'use strict';
+
+export default class UrlUtils {
+    /**
+     * IP地址，域名
+     */
+    static domain = 'http://116.228.114.190:8866';
+    /**
+     * 登录接口
+     */
+    static loginUrl = '/galaxy-sso-business/login';
+
+}
+
+
